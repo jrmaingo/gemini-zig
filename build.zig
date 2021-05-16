d183f520990b6a5263365e21e70bb9539a43c5ee
Julian Maingot <julianmaingot@gmail.com>
2021-05-15 20:22:01 -0700

    Added basic request object

--- build.zig
+++ build.zig
@@ -24,4 +24,10 @@ pub fn build(b: *Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const tests = b.addTest("src/main.zig");
+    tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,50 @@
 const std = @import("std");
+const assert = std.debug.assert;
+const expect = std.testing.expect;
+
+const Request = struct {
+    requestBuf: [requestBufLen]u8,
+    requestLen: usize,
+
+    const Self = @This();
+    const maxUrlLen = 1024;
+    const requestSuffix = "\u{0D}\u{0A}";
+    const requestBufLen = maxUrlLen + requestSuffix.len;
+
+    fn init(url: []const u8) Self {
+        assert(url.len > 0 and url.len <= maxUrlLen);
+
+        const requestLen = url.len + requestSuffix.len;
+        var requestBuf: [requestBufLen]u8 = undefined;
+
+        // TODO handle error
+        _ = std.fmt.bufPrint(requestBuf[0..], "{}{}", .{ url, requestSuffix }) catch unreachable;
+
+        return Self{
+            .requestBuf = requestBuf,
+            .requestLen = url.len + requestSuffix.len,
+        };
+    }
+
+    fn getUrl(self: *const Self) []const u8 {
+        const urlLen = self.requestLen - requestSuffix.len;
+        return self.requestBuf[0..urlLen];
+    }
+
+    fn getBytes(self: *const Self) []const u8 {
+        return self.requestBuf[0..self.requestLen];
+    }
+};
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    // TODO take input instead
+    const dest = "gemini://gemini.circumlunar.space/";
+    const request = Request.init(dest);
+    std.log.info("loading {}", .{request.getUrl()});
+}
+
+test "create request" {
+    const dest = "gemini://gemini.circumlunar.space/";
+    const request = Request.init(dest);
+    expect(std.mem.eql(u8, dest, request.getUrl()));
 }
