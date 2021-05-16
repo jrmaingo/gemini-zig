cfb466077055b6bdc8de1a3e49305455f261ff54
Julian Maingot <julianmaingot@gmail.com>
2021-05-16 09:51:12 -0700

    More mbedtls config and setup

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,9 @@ const assert = std.debug.assert;
 const expect = std.testing.expect;
 const c = @cImport({
     @cInclude("mbedtls/ssl.h");
+    @cInclude("mbedtls/net_sockets.h");
+    @cInclude("mbedtls/entropy.h");
+    @cInclude("mbedtls/ctr_drbg.h");
 });
 
 const defaultPort: u16 = 1965;
@@ -66,17 +69,53 @@ pub fn main() anyerror!void {
     var ssl_config = @ptrCast(*c.mbedtls_ssl_config, configBuf);
     c.mbedtls_ssl_config_init(ssl_config);
     var res = c.mbedtls_ssl_config_defaults(ssl_config, c.MBEDTLS_SSL_IS_CLIENT, c.MBEDTLS_SSL_TRANSPORT_STREAM, c.MBEDTLS_SSL_PRESET_DEFAULT);
-    if (res < 0) {
+    if (res != 0) {
         return GeminiError.Unknown;
     }
     defer c.mbedtls_ssl_config_free(ssl_config);
 
-    res = c.mbedtls_ssl_setup(&ssl_ctx, ssl_config);
-    if (res < 0) {
+    const c_dest: [:0]const u8 = "gemini.circumlunar.space";
+    const c_port: [:0]const u8 = "1965";
+    res = c.mbedtls_ssl_set_hostname(&ssl_ctx, c_dest);
+    if (res != 0) {
         return GeminiError.Unknown;
     }
-    //var socket = try std.net.tcpConnectToHost(allocator, dest, defaultPort);
-    //defer socket.close();
+
+    var socket: c.mbedtls_net_context = undefined;
+    c.mbedtls_net_init(&socket);
+    res = c.mbedtls_net_connect(&socket, c_dest, c_port, c.MBEDTLS_NET_PROTO_TCP);
+    if (res != 0) {
+        std.log.err("socket error: {x}", .{res});
+        return GeminiError.Unknown;
+    }
+    defer c.mbedtls_net_free(&socket);
+
+    c.mbedtls_ssl_set_bio(&ssl_ctx, &socket, c.mbedtls_net_send, c.mbedtls_net_recv, c.mbedtls_net_recv_timeout);
+
+    var entropy_ctx: c.mbedtls_entropy_context = undefined;
+    c.mbedtls_entropy_init(&entropy_ctx);
+    defer c.mbedtls_entropy_free(&entropy_ctx);
+
+    var rng_ctx: c.mbedtls_ctr_drbg_context = undefined;
+    c.mbedtls_ctr_drbg_init(&rng_ctx);
+    res = c.mbedtls_ctr_drbg_seed(&rng_ctx, c.mbedtls_entropy_func, &entropy_ctx, null, 0);
+    if (res != 0) {
+        std.log.err("rng seed error: {x}", .{res});
+        return GeminiError.Unknown;
+    }
+    c.mbedtls_ssl_conf_rng(ssl_config, c.mbedtls_ctr_drbg_random, &rng_ctx);
+    defer c.mbedtls_ctr_drbg_free(&rng_ctx);
+
+    res = c.mbedtls_ssl_setup(&ssl_ctx, ssl_config);
+    if (res != 0) {
+        return GeminiError.Unknown;
+    }
+
+    res = c.mbedtls_ssl_handshake(&ssl_ctx);
+    if (res != 0) {
+        std.log.err("handshake error: {x}", .{res});
+        return GeminiError.Unknown;
+    }
 
     // TODO TLS handshake
 }
